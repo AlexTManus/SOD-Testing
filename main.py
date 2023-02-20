47c9f46dc153359c65aebf3626ab269550082fc0
Alexander Thomas Manus <106741489+AlexTManus@users.noreply.github.com>
2023-02-20 21:32:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def publish_to_slack(webhook, msg):
 def iterate_brokers(broker_list):
     logger.info("|||||||||||||||||||||||||")
     logger.info(broker_list)
+    print(str(type(broker_list)))
     for broker in broker_list:
         (brokerage_id, brokerage_name) = broker
         logger.info(f"Begin Processing of {brokerage_name} with ID: {brokerage_id}")
